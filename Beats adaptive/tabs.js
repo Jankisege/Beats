2d3325584169094b02fe4340e031b71c64d8cd09
Jankisege <140376682+Jankisege@users.noreply.github.com>
2023-10-11 15:22:05 +0300

    3 week 2

--- Beats adaptive/tabs.js
+++ Beats adaptive/tabs.js
@@ -1,9 +1,19 @@
+const findblockbealias = alias =>{
+  return $(".reviews__item").filter((ndx, item)=>{
+    return $(item).attr("data-linked-with") = alias 
+  });
+}
+
+
 $(".interactive__avatar-link").click(e => {
 e.preventDefault();
 
 const $this = $(e.currentTarget);
-const curItem = $this.closest("reviews__switcher-item")
+const target = $this.attr("data-open");
+const itemToShow = findBlockByAlias (target);
+const curItem = $this.closest("reviews__switcher-item");
 
+itemToShow.addclass("active").siblings().removeClass("active");
 curItem.addclass("active").siblings().removeClass("active");
 
 
